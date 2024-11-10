051f1e6757a0441ff0977c248232d62b1da75431
Toshinobu Nakamura <nkmr104@outlook.com>
2024-11-10 15:10:53 +0000

    create class and instance, but give up on passing argument as the structure is not suitable

--- tab.js
+++ tab.js
@@ -1,44 +1,50 @@
 (() => {
-    const $doc = document;
-    const $tab = $doc.getElementById("js-tab");
-    const $nav = $tab.querySelectorAll("[data-nav]");
-    const $content = $tab.querySelectorAll("[data-content]");
-    const ACTIVE_CLASS = "is-active"
-    const navLen = $nav.length;
+    class createTab {
+        constructor() {
+            const $doc = document;
+            this.$tab = $doc.getElementById("js-tab");
+            this.$nav = this.$tab.querySelectorAll("[data-nav]");
+            this.$content = this.$tab.querySelectorAll("[data-content]");
+            this.ACTIVE_CLASS = "is-active"
+            this.navLen = this.$nav.length;
 
-    const init = () => {
-        $content[0].style.display = "block";
-    };
-    init();
+            this.init();
 
-    const clickHandler = (e) => {
-        e.preventDefault();
+            let index = 0;
+            while (index < this.navLen) {
+                this.$nav[index].addEventListener("click", (e) => this.clickHandler(e));
+                index++;
+            }
 
-        //クリックされたnavとそのdataを取得
-        const $this = e.target;
-        const targetVal = $this.dataset.nav;
-
-        //contentとnavを全てリセットする
-        let index = 0;
-        while (index < navLen) {
-            $content[index].style.display = "none";
-            $nav[index].classList.remove(ACTIVE_CLASS);
-            index++;
         }
 
-        // 対象のコンテンツをアクティブ化する
-        $tab.querySelectorAll(`[data-content="${targetVal}"]`)[0].style.display = 'block';
-        $nav[targetVal].classList.add(ACTIVE_CLASS);
+        init() {
+            this.$content[0].style.display = "block";
+        };
 
-        console.log(targetVal);
-
+        clickHandler(e) {
+            e.preventDefault();
+                
+            //クリックされたnavとそのdataを取得
+            const $this = e.target;
+            const targetVal = $this.dataset.nav;
+    
+            //contentとnavを全てリセットする
+            let index = 0;
+            while (index < this.navLen) {
+                this.$content[index].style.display = "none";
+                this.$nav[index].classList.remove(this.ACTIVE_CLASS);
+                index++;
+            }
+    
+            // 対象のコンテンツをアクティブ化する
+            this.$tab.querySelectorAll(`[data-content="${targetVal}"]`)[0].style.display = 'block';
+            this.$nav[targetVal].classList.add(this.ACTIVE_CLASS);
+    
+        }
     }
 
-    let index = 0;
-    while (index < navLen) {
-        $nav[index].addEventListener("click", (e) => clickHandler(e));
-        index++;
-    }
+    new createTab();
 
 }
 
